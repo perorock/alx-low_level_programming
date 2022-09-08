aae07eb166e2204322ddf56916237f287489a5e7
perorock <peter4idea@gmail.com>
2022-09-08 14:23:56 -0700

    aspect

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - To print the string in the put function
+ *
+ * Description: This program uses the main function
+ * The program is intended to print "Programming is like building a multilingual puzzle"
+ * Return: 0
+* * /
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
+
