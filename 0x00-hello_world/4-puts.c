bc2f582d4f76f760c6fcaa71976e40844c62e70d
perorock <peter4idea@gmail.com>
2022-09-08 14:51:07 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
  * main - To print the string in the put function
  *
  * Description: This program uses the main function
- * The program is intended to print "Programming is like building a multilingual puzzle"
+ * this program is intended to print "Programming is like building a multilingual puzzle"
  * Return: 0
-* * /
+ * /
 int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle);
 	return (0);
 
 }
