889fa38f1903141b6eee4da1784a328fd6261ab9
perorock <peter4idea@gmail.com>
2022-09-08 21:51:26 -0700

    compiled updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
- * main - To print the string in the put function
+ * main - program entry
  *
- * Return: zero
+ * Return: always one
  */
- int main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
 
