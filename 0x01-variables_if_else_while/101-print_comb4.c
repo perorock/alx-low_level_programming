2b7fd5752c25c389422ea5f986177b7cba87f1bc
perorock <peter4idea@gmail.com>
2022-09-09 22:39:28 -0700

    combination of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combination of 3 digits
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	int val1, val2, val3;
+
+	for (val1 = 48; val1 < 58; val1++)
+	{
+		for (val2 = 49; val2 < 58; val2++)
+		{
+			for (val3 = 50; val3 < 58; val3)
+			{
+				if (val3 > val2 && val2 > val1)
+				{
+					putchar(val1);
+					putchar(val2);
+					putchar(val3);
+					if (val1 != 55 || val2 != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
