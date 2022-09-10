56c8adf55ab6f8b078474c7d52015854795c9e86
perorock <peter4idea@gmail.com>
2022-09-09 21:18:43 -0700

    updated file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 99; i++)
+	for (i = 1; i <= 89; i++)
 	{
 		putchar((i / 10) + '0');
 		putchar((i % 10) + '0');
-		if (i == 99)
+		if (i == 89)
 			break;
 		putchar(',');
 		putchar(' ');
