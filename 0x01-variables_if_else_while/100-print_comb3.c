2a88db7fb6171696aaa34dc9815524e949080985
perorock <peter4idea@gmail.com>
2022-09-09 21:48:00 -0700

    task 10 redo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		putchar((i / 10) + '0');
 		putchar((i % 10) + '0');
-		if (i != 99)
+		if (i == 89)
 			break;
 		putchar(',');
 		putchar(' ');
