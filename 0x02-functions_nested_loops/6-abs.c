778dd100aca59335213cd89becfa006b69a77efa
perorock <peter4idea@gmail.com>
2022-09-13 18:53:25 -0700

    task 6 to 10

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - computes absolute value of an integer
+ * @t: paased as argument into the function
+ * Return: 0
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+	{
+		return (t);
+	}
+	else
+		return (c * -1);
+}
+
+
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - function that prints the last digits of a number
+ * @t: passed t as an argument into the function
+ * Return: 0
+ */
+int print_last_digit(int t)
+{
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+	_putchar(last_digit + '0');
+	return (last_digit);
+}
