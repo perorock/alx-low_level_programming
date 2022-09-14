5f496dcd2029af75dd089083fd74c9803109d969
perorock <peter4idea@gmail.com>
2022-09-13 17:58:38 -0700

    numbers to print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,7 +11,14 @@ int print_sign(int t)
 		_putchar('+');
 		return (1);
 	}
+	else if (t < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
 	else
+	{
 		_putchar('0');
-	return (0);
+		return (0);
+	}
 }
