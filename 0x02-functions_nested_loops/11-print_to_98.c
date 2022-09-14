8c2772b5338a01040160eab24f73ee2f21f1c048
perorock <peter4idea@gmail.com>
2022-09-13 19:33:34 -0700

    To print natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_to_98 - to print all natural numbers
+ * @t: counter
+ * Return: 0
+ */
+void print_to_98(int t);
+{
+	if (t >= 98)
+	{
+		while (t > 98)
+			printf("%d, ", t--);
+		printf("%d\n", t);
+	}
+	else
+	{
+		while (t < 98)
+			printf("%d, ", t++);
+		printf("%d\n", t);
+	}
+}
