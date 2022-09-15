4bcb34573ebed7a054589b4b82a1335310ac6f5d
perorock <peter4idea@gmail.com>
2022-09-14 19:45:36 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,11 +17,14 @@ void times_table(void)
 			_putchar(' ');
 			prod = num * mult;
 			if (prod <= 9)
+			{
 				_putchar(' ');
+			}
 			else
+			{
 				_putchar((prod / 10) + '0');
 				_putchar((prod % 10) + '0');
-		}
-		_putchar('\n');
+			}
+			_putchar('\n');
 	}
 }
