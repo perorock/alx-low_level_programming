0795dc930678ff8bb22fd8ffda79f8ae19e6606b
perorock <peter4idea@gmail.com>
2022-09-16 07:33:31 -0700

    main task o0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - main
+ * main - main task
+ *
  * Return: 0
  */
 int main(void)
@@ -10,5 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
