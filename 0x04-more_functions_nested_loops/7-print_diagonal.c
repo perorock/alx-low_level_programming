dc7aabc4770840fb4853a20b26afffddd924d9e5
perorock <peter4idea@gmail.com>
2022-09-15 21:35:59 -0700

    task 7 corrected

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,7 +12,7 @@ void print_diagonal(int n)
 		for (len = 0; len < n; len++)
 		{
 			for (space = 0; space < len; space++)
-				putchar(' ');
+				_putchar(' ');
 			_putchar('\\');
 			if (len == n - 1)
 				continue;
