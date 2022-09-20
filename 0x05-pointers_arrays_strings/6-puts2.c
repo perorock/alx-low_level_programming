9091ba8654abd23845d46da9b4e9846deb023163
perorock <peter4idea@gmail.com>
2022-09-19 18:19:29 -0700

    Task 1 - 6 on pointers, array and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - function should print only one character out of two
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
+	}
+	_putchar('\n');
+}
