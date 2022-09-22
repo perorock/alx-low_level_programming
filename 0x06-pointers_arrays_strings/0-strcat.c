31c64eb4f8a63ef8758aeaf109802bdb20e14fa6
perorock <peter4idea@gmail.com>
2022-09-22 14:10:46 -0700

    Concate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * strcat - use concatenation to join strings
+ * _strcat - use concatenation to join strings
  *
  * @dest: pointr to the string to be concatenate
  * @src: source string
  * Return: A pointer to the destination
  */
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, const char *src)
 {
 	int index = 0, dest_len = 0;
 
