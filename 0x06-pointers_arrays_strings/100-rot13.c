c37cf150f35c3536151063701366d0b2f666398d
perorock <peter4idea@gmail.com>
2022-09-22 20:38:35 -0700

    Advanced task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * rot13 - Encodes a string using rot13
+ * @str: The string to be encoded
+ *
+ * Return: A pointer to the encoded string
+ */
+char *rot13(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		str[i] = transform_2(str[i]);
+		i++;
+	}
+	return (str);
+}
+
+/**
+ * transform_2 - helper function to map a letter with it's rot12 encoding
+ * @x: char to be encoded
+ *
+ * Return: the encoded char
+ */
+char transform_2(char x)
+{
+	char one[52] = "ABCDEFGHIJKLMNOPQRSTUVWXWZ";
+	char two[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0;
+	char replacement = x;
+
+	while (i < 52)
+	{
+		if (x == one[i])
+		{
+			replacement = two[i];
+			break;
+		}
+		i++;
+	}
+	return (replacement);
+}
