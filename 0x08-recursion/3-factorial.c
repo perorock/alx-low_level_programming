dfc8a69bce6f07b38326fd87d8f5ad2160df9a78
perorock <peter4idea@gmail.com>
2022-09-28 19:48:12 -0700

    print factorial of a given number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,10 +6,10 @@
  */
 int factorial(int n)
 {
-	int restult = n;
-
-	if (n < 0)
-		return (-1);
-	else if (n >= 0 && n <= 1)
+	if (n == 0)
 		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (n * factorial(n - 1));
 }
