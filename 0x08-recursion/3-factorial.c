743fc541b248b819bc7a12f401662bbce7df45d0
perorock <peter4idea@gmail.com>
2022-09-28 19:41:33 -0700

    print factorial of a given number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * factorial(int n) - returns the factorial of a given number
- * @n: pointer block of memory to be filled
- * @Return: factorial
+ * factorial - returns the factorial of a given number
+ * @n: the number to find
+ * Return: 0 or 1
  */
 int factorial(int n)
 {
-	if (n == 0)
-		return (1);
-	else if (n < 0)
+	int restult = n;
+
+	if (n < 0)
 		return (-1);
-	else
-		return (n * factorial(n - 1));
+	else if (n >= 0 && n <= 1)
+		return (1);
 }
