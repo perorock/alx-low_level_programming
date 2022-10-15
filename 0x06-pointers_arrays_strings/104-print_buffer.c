ff620ea16981209e6a6ec959cdc640b417d8148a
perorock <peter4idea@gmail.com>
2022-10-14 21:59:00 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_buffer - Prints a buffer 10 btyes at a time 
+ * @b: The buffer to be printed
+ * @size: The number of bytes to be printed 
+ */
+void print_buffer(char *b, int size)
+{
+	int byte, index;
+
+	for (byte = 0; byte < size; byte += 10)
+	{
+		printf("%08x: ", byte);
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
+				printf(" ");
+			else
+				printf("%02x", *(b + index + byte));
+			if ((index % 2) != 0 && index != 0)
+				printf(" ");
+		}
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
+				break;
+			else if (
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# alx-low_level_programming
-C Language Programming
+0x10 task
